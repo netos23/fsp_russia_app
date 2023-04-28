de045f58336595343a09ff8e96dea7d0dcdfa4ab
Ушаков Владимир <eau120676@gmail.com>
2023-04-28 09:57:19 +0300

    fixed dark mode

--- lib/screens/rating_screen/rating_screen_presenter.dart
+++ lib/screens/rating_screen/rating_screen_presenter.dart
@@ -1,4 +1,17 @@
 class RatingScreenPresenter {
+  final data = <List<String>>[
+    ["CodeNinja", "1254", "Москва"],
+    ["ByteMaster", "1023", "Санкт-Петербург"],
+    ["ProgChampion", "984", "Новосибирск"],
+    ["AlgorithmWizard", "953", "Екатеринбург"],
+    ["Hackathlon", "835", "Нижний Новгород"],
+    ["CodeWarrior", "832", "Казань"],
+    ["ByteBlazer", "730", "Челябинск"],
+    ["ProgGenius", "734", "Омск"],
+    ["CodeJuggernaut", "732", "Самара"],
+    ["CyberSherlock", "706", "Ростов-на-Дону"],
+  ];
+
 
   void toUserProfile() {
   }
